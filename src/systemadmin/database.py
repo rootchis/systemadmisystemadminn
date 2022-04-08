07e398f9484cab19350985bc65e12019d3c88d5c
Rodrigue@FITEC <yango2903@gmail.com>
2022-04-08 16:18:19 +0200

    Update with CI-CD

--- src/systemadmin/database.py
+++ src/systemadmin/database.py
@@ -4,7 +4,6 @@ from sqlite3 import Error
 #import pandas as pd
 
 
-
 class Databaz:
     def __init__(self):
         print(f"Runnning Database with sqlite: {sqlite3.version}")
@@ -17,6 +16,7 @@ class Databaz:
             pass
 
     def createdb(self, dbfile, table):
+        print(f"create DB{dbfile}")
         table = table
         conn = None
         try:
@@ -27,7 +27,25 @@ class Databaz:
             if conn:
                 conn.close()
 
-    dbz = Databaz()
 
-    dbz.createdb("../../tests/resources/db/pythonsqlite.db", "")
+sql_create_projects_table = """ CREATE TABLE IF NOT EXISTS projects (
+                                        id integer PRIMARY KEY,
+                                        name text NOT NULL,
+                                        begin_date text,
+                                        end_date text
+                                    ); """
+
+sql_create_tasks_table = """CREATE TABLE IF NOT EXISTS tasks (
+                                    id integer PRIMARY KEY,
+                                    name text NOT NULL,
+                                    priority integer,
+                                    status_id integer NOT NULL,
+                                    project_id integer NOT NULL,
+                                    begin_date text NOT NULL,
+                                    end_date text NOT NULL,
+                                    FOREIGN KEY (project_id) REFERENCES projects (id)
+                                );"""
+
+dbz = Databaz()
+dbz.createdb("../../tests/resources/db/pythonsqlite.db", "")
 
